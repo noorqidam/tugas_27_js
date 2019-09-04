56eabed089c72aedb0d5a7282b5976a4352e9be8
noorqidam <noorqidam@gmail.com>
2019-09-04 09:41:52 +0700

    Tugas 27 JS
    Tugas ini saya kumpulkan untuk Niomic

---
+++ tugas27.js
@@ -0,0 +1,20 @@
+function tugas27A() {
+
+  var a = "No";
+  var b = "rumah saya";
+  var c = 30;
+  var d = "A"
+
+  gabungan = a + " " + b + " " + c + d;
+  console.log(gabungan);
+}
+tugas27A();
+
+function tugas27B() {
+  var a = "Jalan pangeran Tirtayasa No";
+  var b = 70;
+
+  gabungan = a + " " + b;
+  console.log(gabungan);
+}
+tugas27B();
